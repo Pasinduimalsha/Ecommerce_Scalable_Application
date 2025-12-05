23735353e4d4a1f03673051ab7d96b24cfd6afbe
Pasinduimalsha <Imalsha_map_e23@engug.ruh.ac.lk>
2025-12-06 00:27:52 +0530

    Added an auth service

---
+++ Backend/auth-service/src/main/java/com/pasindu/auth_service/controller/webtest.java
@@ -0,0 +1,15 @@
+package com.pasindu.auth_service.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class webtest {
+
+    @GetMapping("test")
+    public String webtest(){
+        return "Hi";
+    }
+}
---
+++ Backend/auth-service/src/main/java/com/pasindu/auth_service/entity/User.java
@@ -0,0 +1,23 @@
+package com.pasindu.auth_service.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+    @Id
+    private Integer id;
+    private String userName;
+    private String password;
+
+}
